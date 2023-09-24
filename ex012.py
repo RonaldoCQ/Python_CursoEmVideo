154faaf77d478b63f353c81921126e1bdc6cc8d0
Ronaldo Carvalho <88563134+RonaldoCQ@users.noreply.github.com>
2023-09-24 12:04:26 -0300

    Add files via upload

---
+++ ex012.py
@@ -0,0 +1,7 @@
+#leia o preço de um produto e aplique um desconto de 5%
+
+preco = int(input('Digite o valor do item que deseja consultar: '))
+
+print('O valor do item ficou em {:.2f} na super promoção oferecida! '.format(preco-(preco*0.05)))
+#Essa é a 13
+print('O valor do novo salário do funcionario é {} '.format(preco+(preco*0.15)))
