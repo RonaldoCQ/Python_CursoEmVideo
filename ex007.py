c99680195a524cd2fb9e09e8ab007d6b06166ab5
Ronaldo Carvalho <88563134+RonaldoCQ@users.noreply.github.com>
2023-09-24 12:09:21 -0300

    Delete ex007.py
    Deleting some problematical files

--- ex007.py
+++
@@ -1,9 +0,0 @@
-#Calcular média de um aluno com base em 2 notas - básico sem SE ou coisas do tipo
-
-n1 = int(input('Digite o valor da primeira nota: '))
-n2 = int(input('Digite o valor da segunda nota: '))
-
-media = (n1+n2)/2
-
-print('A média do aluno é {:3.1f}'.format(media))
-
